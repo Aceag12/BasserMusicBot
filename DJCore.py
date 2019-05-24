196c0258fc8f1ebc5bbbfb8943713ceb589186d3
Aceag12 <46562212+Aceag12@users.noreply.github.com>
2019-05-24 07:53:49 +0545

    Update DJCore.py

--- DJCore.py
+++ DJCore.py
@@ -6,12 +6,7 @@ bot = commands.AutoShardedBot(case_insensitive=True, command_prefix=commands.whe
 bot.remove_command('help')
 bot.initials = ('modules.misc', 'modules.music', 'modules.handler', 'modules.owner')
 bot.owner = int(os.getenv('OWNER'))
-bot.color = int(os.getenv('COLOR'), 16)
-
-@bot.event
-async def on_ready():
-    print(f'Bot is ready! Logged as in: {bot.user}')
-    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=f"BasserMusic | {os.getenv('PREFIX')}help"))                                                                                  
+bot.color = int(os.getenv('COLOR'), 16)                                                                                  
                                                                                    
 if __name__ == "__main__":
     for extension in bot.initials:
