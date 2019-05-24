8473e0d56b5b27e31b9b150864f4dabd07a02671
Aceag12 <46562212+Aceag12@users.noreply.github.com>
2019-05-24 07:50:57 +0545

    Update DJCore.py

--- DJCore.py
+++ DJCore.py
@@ -11,11 +11,7 @@ bot.color = int(os.getenv('COLOR'), 16)
 @bot.event
 async def on_ready():
     print(f'Bot is ready! Logged as in: {bot.user}')
-    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=f"BasserMusic | {os.getenv('PREFIX')}help"))
-
-@bot.check
-async def _bot_protection(ctx):
-    return not ctx.author.bot                                                                                   
+    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=f"BasserMusic | {os.getenv('PREFIX')}help"))                                                                                  
                                                                                    
 if __name__ == "__main__":
     for extension in bot.initials:
